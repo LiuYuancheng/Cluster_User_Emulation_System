9f710e5ea85a5fd9db1532de22b8b8a1938d4e5a
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2022-12-16 09:13:34 +0800

    Added the download action.

--- src/actorFunctions.py
+++ src/actorFunctions.py
@@ -10,13 +10,17 @@
 # License:     
 #-----------------------------------------------------------------------------
 import os
+import time
 from random import randint
 import actionGlobal as gv
-from UtilsFunc import pingActor
-from UtilsFunc import funcActor
+from urllib.parse import urljoin, urlparse
+from UtilsFunc import pingActor, funcActor, zoomActor, webDownload
+
 import Log
 import SSHconnector
 
+
+PORT = 443 # port to download the server certificate most server use 443.
 
 #-----------------------------------------------------------------------------
 def func_0901():
@@ -102,18 +106,49 @@ def func_0932():
     
 #-----------------------------------------------------------------------------
 def func_0935():
-    appName = 'zoomActor.py'
-    appPath = os.path.join(gv.ACTOR_DIR, appName)
-    cmd = "python %s" %str(appPath)
-    result = funcActor.runCmd(cmd)
-    print(result)
+    # start a zoom meeting
+    #appName = 'zoomActor.py'
+    #appPath = os.path.join(gv.ACTOR_DIR, appName)
+    #cmd = "python %s" %str(appPath)
+    #result = funcActor.runCmd(cmd)
+    #print(result)
+    actor = zoomActor.zoomActor(userName='TestUser_Bob')
+    actor.startMeeting('https://us04web.zoom.us/j/4580466160?pwd=d0ZUSCs0bWpMc2o2MHgzTS80a2tJdz09')
+    meetingPeriod = 20 # 20 mins meeting
+    time.sleep(60*meetingPeriod)
+    actor.endCrtMeeting()
+    print("Finish")
 
+#-----------------------------------------------------------------------------
+def func_1015():
+    # download some web contents based on the url config. 
+    soup = webDownload.urlDownloader(imgFlg=True, linkFlg=True, scriptFlg=True, caFlg=True)
+    count = failCount= 0
+    if not os.path.exists(gv.RST_DIR): os.mkdir(gv.RST_DIR)
+    soup.setResutlDir(gv.RST_DIR)
+    print("> load url record file %s" %gv.URL_RCD)
+    with open(gv.URL_RCD) as fp:
+        urllines = fp.readlines()
+        for line in urllines:
+            if line[0] in ['#', '', '\n', '\r', '\t']: continue # jump comments/empty lines.
+            count += 1
+            print("> Process URL {}: {}".format(count, line.strip()))
+            if ('http' in line):
+                line = line.strip()
+                domain = str(urlparse(line).netloc)
+                folderName = "_".join((str(count), domain))
+                result = soup.savePage(line, folderName)
+                # soup.savePage('https://www.google.com', 'www_google_com')
+                if result: 
+                    print('Finished.')
+                else:
+                    failCount +=1
+    print("\n> Download result: download %s url, %s fail" %(str(count), str(failCount)))
 
 
 #-----------------------------------------------------------------------------
-
 def testCase(mode):
-    func_0935()
+    func_1015()
 
 if __name__ == '__main__':
     testCase(1)
