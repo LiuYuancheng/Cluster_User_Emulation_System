e34399873d141f23724cb7d760a722bbc9172b18
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2023-01-03 15:54:05 +0800

    Added the front end web host program.

---
+++ src/monitorHub/frontend/app.py
@@ -0,0 +1,68 @@
+#!/usr/bin/python
+#-----------------------------------------------------------------------------
+# Name:        app.py [python3]
+#
+# Purpose:     This module is the main website host program to host the webpage 
+#              by using Flask frame work. 
+#  
+# Author:      Yuancheng Liu, Andy Lam Wei Jie
+#
+# Created:     2022/08/27
+# version:     v0.2
+# Copyright:   National Cybersecurity R&D Laboratories
+# License:     
+#-----------------------------------------------------------------------------
+
+# CSS lib [bootstrap]: https://www.w3schools.com/bootstrap4/default.asp
+
+from datetime import timedelta, datetime
+from http import server
+from flask import Flask, render_template, request, flash, url_for, redirect
+from flask_login import LoginManager, login_required, current_user
+from werkzeug.exceptions import abort
+
+import frontendGlobal as gv
+
+# Init the flask web app program.
+def createApp():
+    """ Connect to the monitor hub server and init the flask.app.
+        Returns:
+            _type_: flask.app()
+    """
+    print("Check whether can connect to the monitor-hub backend server:")
+    
+    # Try to connect to the monitor-hub backend server.
+    #gv.iDataMgr = dataManager.DataManager(None)
+    #if not gv.iDataMgr: exit()
+    #gv.iDataMgr.start()
+
+    # init the web host
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = gv.APP_SEC_KEY
+    app.config['REMEMBER_COOKIE_DURATION'] = timedelta(seconds=gv.COOKIE_TIME)
+    return app
+
+app = createApp()
+
+#-----------------------------------------------------------------------------
+# web home request handling functions.
+@app.route('/')
+def index():
+    #Log.info('/index.html is accessed from IP: %s' %str(request.remote_addr))
+    posts = [{
+        'id': 1,
+        'name': "09:01_ping",
+        'detail':"Ping 100+ destinations",
+        'owner': "User:LYC",
+        'startT': "Every 1 day at 09:01:00",
+        'period': '10 mins',
+        'type':0,
+        'dep':0,
+        'state':1,
+        'nextT': "2023-01-04 09:01:00"
+    }]
+    return render_template('index.html', posts=posts)
+
+#-----------------------------------------------------------------------------
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=5000,  debug=False, threaded=True)
---
+++ src/monitorHub/frontend/frontendGlobal.py
@@ -0,0 +1,89 @@
+#-----------------------------------------------------------------------------
+# Name:        frontGlobal.py
+#
+# Purpose:     This module is used as a local config file to set constants, 
+#              global parameters which will be used in the other modules.
+#              
+# Author:      Yuancheng Liu
+#
+# Created:     2022/08/26
+# Copyright:   National Cybersecurity R&D Laboratories
+# License:     
+#-----------------------------------------------------------------------------
+"""
+For good coding practice, follow the following naming convention:
+    1) Global variables should be defined with initial character 'g'
+    2) Global instances should be defined with initial character 'i'
+    2) Global CONSTANTS should be defined with UPPER_CASE letters
+"""
+
+import os, sys
+
+print("Current working directory is : %s" % os.getcwd())
+dirpath = os.path.dirname(__file__)
+print("Current source code location : %s" % dirpath)
+APP_NAME = ('NCL_BM_HUB', 'frontend')
+
+TOPDIR = 'src'
+LIBDIR = 'lib'
+
+idx = dirpath.find(TOPDIR)
+gTopDir = dirpath[:idx + len(TOPDIR)] if idx != -1 else dirpath   # found it - truncate right after TOPDIR
+# Config the lib folder 
+gLibDir = os.path.join(gTopDir, LIBDIR)
+if os.path.exists(gLibDir):
+    sys.path.insert(0, gLibDir)
+import Log
+Log.initLogger(gTopDir, 'Logs', APP_NAME[0], APP_NAME[1], historyCnt=100, fPutLogsUnderDate=True)
+
+#------<CONSTANTS>-------------------------------------------------------------
+APP_NAME = 'NCL_BM_HUB [Ver:0.X]'
+
+RC_TIME_OUT = 10    # reconnection time out.
+BE_IP = ('127.0.0.1', 3001)     # backend server IP address.
+APP_SEC_KEY = 'secrete-key-goes-here'
+
+# define the keys
+# user keys
+USER_ID_KEY = 'id'
+USER_EM_KEY = 'email'
+USER_NA_KEY = 'userName'
+USER_TP_KEY = 'authority'
+USER_PW_KEY = 'password'
+
+# CPU/GPU keys
+ITEM_ID_KEY = 'id'
+ITEM_SN_KEY = 'servername'
+ITEM_IP_KEY = 'ipaddress'
+
+ITEM_USR_KEY = 'userID'
+ITEM_TM_KEY = 'updateT'
+ITEM_STM_KEY = 'startT'
+ITEM_ETM_KEY = 'endT'
+ITEM_RQST_KEY = 'requestStatus'
+
+GPU_TP_KEY = 'gpuType'
+GPU_BK_KEY = 'bookID'
+GPU_LD_KEY = 'gpuLoad'
+
+
+ITEM_PWR_KEY = 'poweron' # Power on/off, for CPU
+ITEM_PWRLVL_KEY = 'powerlvl' # Power level, for GPU
+
+ITEM_LD_KEY = 'overload'
+ITEM_CPULD_KEY = 'cpuload'
+ITEM_MMRLD_KEY = 'memoryload'
+
+ITEM_OS_KEY = 'ostype'
+ITEM_GPU1_KEY = 'gpu1'
+ITEM_GPU2_KEY = 'gpu2'
+
+UPDATE_PERIODIC = 15
+COOKIE_TIME = 30
+#-------<GLOBAL VARIABLES (start with "g")>-------------------------------------
+# VARIABLES are the built in data type.
+
+#-------<GLOBAL INSTANCES (start with "i")>-------------------------------------
+# INSTANCES are the object.
+iConnector = None
+iDataMgr = None
