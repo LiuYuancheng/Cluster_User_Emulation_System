65e0afe9e3f9543301e27f1920ef87270f888262
Liu Yuancheng <liu_yuan_cheng@hotmail.com>
2023-01-12 17:57:50 +0800

    Update the dataBase handler.

--- src/dbHandler.py
+++ src/dbHandler.py
@@ -1,43 +1,59 @@
+#-----------------------------------------------------------------------------
+# Name:        dbBaseHandler.py
+#
+# Purpose:     This program is used to reset the dataBase and test the querys.
+#              
+# Author:      Yuancheng Liu 
+#
+# Version:     v_0.2
+# Created:     2023/01/11
+# Copyright:   
+# License:     
+#-----------------------------------------------------------------------------
 
 import sqlite3
 from datetime import datetime, timedelta
 
-import actionGlobal as gv
+from actionGlobal import DB_PATH, SQL_PATH
 
-mode = 0
-connection = sqlite3.connect(gv.DB_PATH)
+connection = sqlite3.connect(DB_PATH)
 
-if mode == 0:
-    print("Clean and reset the data jobs table.")
-    with open(gv.SQL_PATH) as fh:
+def resetDB():
+    print("Clean and reset all the tables.")
+    with open(SQL_PATH) as fh:
         connection.executescript(fh.read())
-    cur = connection.cursor()
-
-elif mode == 1:
-    print("Clean and reset the data jobs table.")
-    with open(gv.SQL_PATH) as fh:
-        connection.executescript(fh.read())
-    cur = connection.cursor()
-    actId = 1
-    actName = '09:01_ping'
-    actDetail = 'Ping 30 destinations'
-    actDesc = ''
-    actOwner = ''
-    actType = 1
-    startT = '09:01:00'
-    depend = 0
-    threadType = 1
-    actState = 'pending'
-    tomorrow = datetime.now() + timedelta(1)
-    nextT =  tomorrow.strftime('%Y-%m-%d') + ' ' + startT
     
-    cur.execute('INSERT INTO dailyActions \
-        (actId, actName, actDetail, actDesc, actOwner, actType, startT, depend, threadType, actState, nextT)\
-        VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
-        (actId, actName, actDetail, actDesc, actOwner, actType, startT, depend, threadType, actState, nextT))
+def testCase(mode):
 
+    if mode == 0:
+        resetDB()
+    elif mode == 1:
+        print("Reset DB and test insert data")
+        resetDB()
+        cur = connection.cursor()
+        actId = 1
+        actName = '09:01_ping'
+        actDetail = 'Ping 30 destinations'
+        actDesc = ''
+        actOwner = ''
+        actType = 1
+        startT = '09:01:00'
+        depend = 0
+        threadType = 1
+        actState = 'pending'
+        tomorrow = datetime.now() + timedelta(1)
+        nextT =  tomorrow.strftime('%Y-%m-%d') + ' ' + startT
+        
+        cur.execute('INSERT INTO dailyActions \
+            (actId, actName, actDetail, actDesc, actOwner, actType, startT, depend, threadType, actState, nextT)\
+            VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+            (actId, actName, actDetail, actDesc, actOwner, actType, startT, depend, threadType, actState, nextT))
+    else:
+        pass
+        # Add your test code here and change the mode part to active it.
+    connection.commit()
+    connection.close()
 
-
-
-connection.commit()
-connection.close()
+if __name__ == '__main__':
+    mode = 1
+    testCase(mode)
